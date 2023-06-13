b60c3643a08be13f4249f84014b4cae2375e1b2f
Mohamed Ali <mohamed.ali@kovai.co>
2023-06-13 12:18:43 +0530

    added random guid in identify method

--- JS_files/usage-collector.js
+++ JS_files/usage-collector.js
@@ -1,4 +1,11 @@
+function uuidv4() {
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+}
+
 $(document).ready(function () {
+
     var empmail = localStorage.getItem("userId")
 
     var fullName = empmail.split('@')[0].split('.');
@@ -16,10 +23,9 @@ $(document).ready(function () {
 
     var usageCollector = new churn360.UsageCollector();
     usageCollector.initialize("ZXlKMklqb3hMakFzSW10bGVTSTZJbU15Tm1FeFlqbG1NamswWXpRMk1XUTVZVFpsTldNek56azVaVFl4T1dZNElpd2lhV1FpT2lKaU5EaGhNR0ZrTW1Fd1pUWTBNakkyT0RZeU1tVTJNR0prTkdRMU5UWXpOU0lzSW5OcElqb2lhRVp4T0hSM1dsbG5iWEZFYm5kR1RUSnBhV2hGYlcxcmVFMW5NRk0zUTI5cFkycDBRMUZsVjI5bE9EMGlmUT09", empmail, empmail);
-    window.addEventListener("load", function () {
-        if (localStorage.getItem("Churn360V-TagApikey")) {
-            usageCollector.identify(traits, UserId);
-            usageCollector.listener();
-        }
-    });
+    if (localStorage.getItem("Churn360-usage-userInfo")) {
+        usageCollector.identify(traits, uuidv4());
+        usageCollector.listener();
+    }
 })
+
